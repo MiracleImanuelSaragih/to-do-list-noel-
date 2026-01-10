6a30cd7d1c451b29af3178b60069bf2f3b5195c4
MiracleImanuelSaragih <miraclemainpb@gmail.com>
2026-01-10 18:03:50 +0700

    Fix Firebase storage bucket configuration
    Updated Firebase storage bucket URL.

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Konfigurasi dari Firebase Console
+// Inisialisasi Firebase (tanpa import)
 const firebaseConfig = {
   apiKey: "AIzaSyAcnTpDBXO2WpXs3ri21SXJfzUMC_xkCkU",
   authDomain: "premium-todo-list.firebaseapp.com",
   projectId: "premium-todo-list",
-  storageBucket: "premium-todo-list.firebasestorage.app",
+  storageBucket: "premium-todo-list.appspot.com",
   messagingSenderId: "630693485104",
   appId: "1:630693485104:web:820a99e6af935a7345abf5"
 };
 
-// Inisialisasi Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const app = initializeApp(firebaseConfig);
